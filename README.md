8999573ed7da0798cec5ba267c54b786b8983d85
Nathan Taylor <nbtaylor@gmail.com>
2020-12-02 13:24:14 -0700

    Migrate to Testify for unit tests

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 A simple intention lock implementation for Go.
 
+## Testing
+
+The test framework includes some randomised load tests
+that are worth running with the race detector on.
+
+```
+$ go test -race -v
+```
+
--- ilock_test.go
+++ ilock_test.go
@@ -7,35 +7,46 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHighConcurrentWrites(t *testing.T) {
-	testLocking(t, 20, 10)
+var workloads = []struct {
+	name        string
+	concurrency int
+	writeRatio  float32
+}{
+	{"Serial", 1, 0.10},
+	{"Serial, heavy writes", 1, 0.50},
+	{"Low concurrency", 2, 0.10},
+	{"Medium concurrency", 10, 0.10},
+	{"High concurrency", 20, 0.10},
+	{"High concurrency, heavy writes", 20, 0.50},
 }
 
-func TestHighConcurrency(t *testing.T) {
-	testLocking(t, 20, 1)
-}
-func TestMedConcurrency(t *testing.T) {
-	testLocking(t, 5, 1)
-}
-
-func TestLowConcurrency(t *testing.T) {
-	testLocking(t, 2, 1)
+func TestWorkloads(t *testing.T) {
+	for _, w := range workloads {
+		t.Logf("Testing workload %v\n", w.name)
+		values := testLocking(t, w.concurrency, int(w.writeRatio*100))
+		testNonDecreasing(t, values)
+	}
 }
 
-func TestSerialHeavyWrites(t *testing.T) {
-	testLocking(t, 1, 20)
-}
-func TestSerial(t *testing.T) {
-	testLocking(t, 1, 1)
+/* Ensure the values are nondecreasing.  If there is a non-decreasing
+* value, because each writer should take some lock at some index and
+* increment all subsequent indices too, if there's a decreasing value
+* observed then we know we're not linearizing our write operations. */
+func testNonDecreasing(t *testing.T, values []uint32) {
+	for i := 1; i < len(values); i++ {
+		assert.LessOrEqual(t, values[i-1], values[i], "Nondecreasing value")
+	}
 }
 
 /* This test simulates `concurrency` actors acting on a "branch"
  * of a tree of data.  mutexes[i] is responsible explicitly for
  * values[i] and all subsequent values, implicitly.
  */
-func testLocking(t *testing.T, concurrency int, writePerc int) {
+func testLocking(t *testing.T, concurrency int, writePerc int) []uint32 {
 	l := log.New(os.Stderr, "", 0)
 	l.SetOutput(ioutil.Discard)
 	barrier := make(chan bool, concurrency)
@@ -77,7 +88,6 @@ func testLocking(t *testing.T, concurrency int, writePerc int) {
 	}
 
 	sHandler := func(offset int, delay time.Duration) {
-		var garbage uint32
 		for i := 0; i < offset; i++ {
 			mutexes[i].ISLock()
 			l.Printf("sHandler -> IS %d %d\n", i, offset)
@@ -85,9 +95,7 @@ func testLocking(t *testing.T, concurrency int, writePerc int) {
 		mutexes[offset].SLock()
 		l.Printf("sHandler -> S %d\n", offset)
 
-		for i := offset; i < len(values); i++ {
-			garbage += values[i]
-		}
+		testNonDecreasing(t, values[:])
 
 		time.Sleep(delay)
 		mutexes[offset].SUnlock()
@@ -108,6 +116,8 @@ func testLocking(t *testing.T, concurrency int, writePerc int) {
 		mutexes[offset].XLock()
 		l.Printf("xHandler -> X %d\n", offset)
 
+		testNonDecreasing(t, values[:])
+
 		for i := offset; i < len(values); i++ {
 			values[i]++
 		}
@@ -127,7 +137,7 @@ func testLocking(t *testing.T, concurrency int, writePerc int) {
 	rng := rand.New(rand.NewSource(seed))
 	for time.Now().Before(end) {
 		rw := rng.Intn(100) < writePerc
-		delay := time.Duration(100+rng.Intn(5000)) * time.Microsecond
+		delay := time.Duration(1+rng.Intn(50)) * time.Millisecond
 		i := rng.Intn(len(mutexes))
 
 		barrier <- true
@@ -152,17 +162,15 @@ func testLocking(t *testing.T, concurrency int, writePerc int) {
 		select {
 		case <-barrier:
 		default:
-			/* Ensure the values are nondecreasing. */
-			v := values[0]
-			t.Logf("Final values: %v\n", values)
-			for i := 1; i < len(values); i++ {
-				if values[i] < v {
-					t.Errorf("Nondecreasing value at index %d\n", i)
-					v = values[i]
-					break
-				}
-			}
-			return
+			// Looks like the race detector spuriously complains about a benign race
+			// as we read the elements in values without being protected by a lock
+			// in the caller. (I'm assuming that the Go race detector does an Eraser-style
+			// race detection procedure where it looks for an empty intersection of
+			// locks held on accesses).
+			mutexes[0].XLock()
+			ret := append([]uint32(nil), values[:]...)
+			mutexes[0].XUnlock()
+			return ret
 		}
 	}
 }
@@ -173,20 +181,12 @@ func TestExtractIXIdempotency(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		state := rng.Uint64()
 		val := rng.Uint64() & maxHolders
-
 		newState := setIX(state, val)
-		if extractIX(newState) != val {
-			t.Errorf("expected %016x; got %016x", val, extractIX(newState))
-		}
-		if extractIS(newState) != extractIS(state) {
-			t.Errorf("expected %016x; got %016x", extractIS(state), extractIS(newState))
-		}
-		if extractS(newState) != extractS(state) {
-			t.Errorf("expected %016x; got %016x", extractIS(state), extractIS(newState))
-		}
-		if extractX(newState) != extractX(state) {
-			t.Errorf("expected %016x; got %016x", extractIS(state), extractIS(newState))
-		}
+
+		assert.Equal(t, val, extractIX(newState), "expected %016x; got %016x", val, extractIX(newState))
+		assert.Equal(t, extractIS(newState), extractIS(state), "expected %016x; got %016x", extractIS(state), extractIS(newState))
+		assert.Equal(t, extractS(newState), extractS(state), "expected %016x; got %016x", extractIS(state), extractIS(newState))
+		assert.Equal(t, extractX(newState), extractX(state), "expected %016x; got %016x", extractIS(state), extractIS(newState))
 	}
 }
 func TestExtractISIdempotency(t *testing.T) {
@@ -197,18 +197,10 @@ func TestExtractISIdempotency(t *testing.T) {
 		val := rng.Uint64() & maxHolders
 
 		newState := setIS(state, val)
-		if extractIS(newState) != val {
-			t.Errorf("expected %016x; got %016x", val, extractIS(newState))
-		}
-		if extractIX(newState) != extractIX(state) {
-			t.Errorf("expected %016x; got %016x", extractIX(state), extractIX(newState))
-		}
-		if extractS(newState) != extractS(state) {
-			t.Errorf("expected %016x; got %016x", extractS(state), extractS(newState))
-		}
-		if extractX(newState) != extractX(state) {
-			t.Errorf("expected %016x; got %016x", extractX(state), extractX(newState))
-		}
+		assert.Equal(t, extractIS(newState), val, "expected %016x; got %016x", val, extractIS(newState))
+		assert.Equal(t, extractIX(newState), extractIX(state), "expected %016x; got %016x", extractIX(state), extractIX(newState))
+		assert.Equal(t, extractS(newState), extractS(state), "expected %016x; got %016x", extractS(state), extractS(newState))
+		assert.Equal(t, extractX(newState), extractX(state), "expected %016x; got %016x", extractX(state), extractX(newState))
 	}
 }
 func TestExtractSIdempotency(t *testing.T) {
@@ -219,18 +211,10 @@ func TestExtractSIdempotency(t *testing.T) {
 		val := rng.Uint64() & maxHolders
 
 		newState := setS(state, val)
-		if extractS(newState) != val {
-			t.Errorf("expected %016x; got %016x", val, extractS(newState))
-		}
-		if extractIX(newState) != extractIX(state) {
-			t.Errorf("expected %016x; got %016x", extractIX(state), extractIX(newState))
-		}
-		if extractIS(newState) != extractIS(state) {
-			t.Errorf("expected %016x; got %016x", extractIS(state), extractIS(newState))
-		}
-		if extractX(newState) != extractX(state) {
-			t.Errorf("expected %016x; got %016x", extractX(state), extractX(newState))
-		}
+		assert.Equal(t, extractS(newState), val, "expected %016x; got %016x", val, extractIS(newState))
+		assert.Equal(t, extractIX(newState), extractIX(state), "expected %016x; got %016x", extractIX(state), extractIX(newState))
+		assert.Equal(t, extractIS(newState), extractIS(state), "expected %016x; got %016x", extractS(state), extractS(newState))
+		assert.Equal(t, extractX(newState), extractX(state), "expected %016x; got %016x", extractX(state), extractX(newState))
 	}
 }
 func TestExtractXIdempotency(t *testing.T) {
@@ -241,18 +225,10 @@ func TestExtractXIdempotency(t *testing.T) {
 		val := rng.Uint64() & maxHolders
 
 		newState := setX(state, val)
-		if extractX(newState) != val {
-			t.Errorf("expected %016x; got %016x", val, extractX(newState))
-		}
-		if extractIX(newState) != extractIX(state) {
-			t.Errorf("expected %016x; got %016x", extractS(state), extractS(newState))
-		}
-		if extractIS(newState) != extractIS(state) {
-			t.Errorf("expected %016x; got %016x", extractIS(state), extractIS(newState))
-		}
-		if extractS(newState) != extractS(state) {
-			t.Errorf("expected %016x; got %016x", extractS(state), extractS(newState))
-		}
+		assert.Equal(t, extractX(newState), val, "expected %016x; got %016x", val, extractIS(newState))
+		assert.Equal(t, extractS(newState), extractS(state), "expected %016x; got %016x", extractX(state), extractX(newState))
+		assert.Equal(t, extractIX(newState), extractIX(state), "expected %016x; got %016x", extractIX(state), extractIX(newState))
+		assert.Equal(t, extractIS(newState), extractIS(state), "expected %016x; got %016x", extractS(state), extractS(newState))
 	}
 }
 
@@ -261,39 +237,23 @@ func TestRegisterX(t *testing.T) {
 
 	// X -> X
 	m = New()
-	if !m.registerX() {
-		t.Errorf("Failure to register X state from nascent Mutex")
-	}
-	if m.registerX() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerX(), "Failure to register X state from nascent Mutex")
+	assert.False(t, m.registerX(), "Failure to ensure mutual writer exclusion")
 
 	// X -> S
 	m = New()
-	if !m.registerX() {
-		t.Errorf("Failure to register X state from nascent Mutex")
-	}
-	if m.registerS() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerX(), "Failure to register X state from nascent Mutex")
+	assert.False(t, m.registerS(), "Failure to ensure mutual writer exclusion")
 
 	// X -> IS
 	m = New()
-	if !m.registerX() {
-		t.Errorf("Failure to register X state from nascent Mutex")
-	}
-	if m.registerIS() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerX(), "Failure to register X state from nascent Mutex")
+	assert.False(t, m.registerIS(), "Failure to ensure mutual writer exclusion")
 
 	// X -> IX
 	m = New()
-	if !m.registerX() {
-		t.Errorf("Failure to register X state from nascent Mutex")
-	}
-	if m.registerIX() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerX(), "Failure to register X state from nascent Mutex")
+	assert.False(t, m.registerIX(), "Failure to ensure mutual writer exclusion")
 }
 
 func TestRegisterS(t *testing.T) {
@@ -301,39 +261,23 @@ func TestRegisterS(t *testing.T) {
 
 	// S -> X
 	m = New()
-	if !m.registerS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if m.registerX() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerS(), "Failure to register S state from nascent Mutex")
+	assert.False(t, m.registerX(), "Failure to ensure mutual writer exclusion")
 
 	// S -> S
 	m = New()
-	if !m.registerS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if !m.registerS() {
-		t.Errorf("Failure to allow simultaneously S states")
-	}
+	assert.True(t, m.registerS(), "Failure to register S state from nascent Mutex")
+	assert.True(t, m.registerS(), "Failure to allow simultaneous S states")
 
 	// S -> IS
 	m = New()
-	if !m.registerS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if !m.registerIS() {
-		t.Errorf("Failure to allow simultaneously S and IS states")
-	}
+	assert.True(t, m.registerS(), "Failure to register S state from nascent Mutex")
+	assert.True(t, m.registerIS(), "Failure to allow simultaneous S and IS states")
 
 	// S -> IX
 	m = New()
-	if !m.registerS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if m.registerIX() {
-		t.Errorf("Allows simultaneously S and IX states")
-	}
+	assert.True(t, m.registerS(), "Failure to register S state from nascent Mutex")
+	assert.False(t, m.registerIX(), "Allows simultaneous S and IX states")
 }
 
 func TestRegisterIS(t *testing.T) {
@@ -341,39 +285,23 @@ func TestRegisterIS(t *testing.T) {
 
 	// IS -> X
 	m = New()
-	if !m.registerIS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if m.registerX() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerIS(), "Failure to register IS state from nascent Mutex")
+	assert.False(t, m.registerX(), "Failure to ensure mutual writer exclusion")
 
 	// IS -> S
 	m = New()
-	if !m.registerIS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if !m.registerS() {
-		t.Errorf("Failure to allow simultaneous S and IS states")
-	}
+	assert.True(t, m.registerIS(), "Failure to register IS state from nascent Mutex")
+	assert.True(t, m.registerS(), "Failure to allow simultaneous S and IS states")
 
 	// IS -> IS
 	m = New()
-	if !m.registerIS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if !m.registerIS() {
-		t.Errorf("Failure to allow simultaneous IS states")
-	}
+	assert.True(t, m.registerIS(), "Failure to register IS state from nascent Mutex")
+	assert.True(t, m.registerIS(), "Failure to allow simultaneous IS states")
 
 	// IS -> IX
 	m = New()
-	if !m.registerIS() {
-		t.Errorf("Failure to register S state from nascent Mutex")
-	}
-	if !m.registerIX() {
-		t.Errorf("Failure to allow simultaneous IS and IX states")
-	}
+	assert.True(t, m.registerIS(), "Failure to register IS state from nascent Mutex")
+	assert.True(t, m.registerIX(), "Failure to allow simultaneous IS and IX states")
 }
 
 func TestRegisterIX(t *testing.T) {
@@ -381,37 +309,21 @@ func TestRegisterIX(t *testing.T) {
 
 	// IX -> X
 	m = New()
-	if !m.registerIX() {
-		t.Errorf("Failure to register IX state from nascent Mutex")
-	}
-	if m.registerX() {
-		t.Errorf("Failure to ensure mutual exclusion on the X state")
-	}
+	assert.True(t, m.registerIX(), "Failure to register IX state from nascent Mutex")
+	assert.False(t, m.registerX(), "Failure to ensure mutual writer exclusion")
 
 	// IX -> S
 	m = New()
-	if !m.registerIX() {
-		t.Errorf("Failure to register IX state from nascent Mutex")
-	}
-	if m.registerS() {
-		t.Errorf("Holding IX and S states simultaneously")
-	}
+	assert.True(t, m.registerIX(), "Failure to register IX state from nascent Mutex")
+	assert.False(t, m.registerS(), "Holding IX and S states simultaneously")
 
 	// IX -> IS
 	m = New()
-	if !m.registerIX() {
-		t.Errorf("Failure to register IX state from nascent Mutex")
-	}
-	if !m.registerIS() {
-		t.Errorf("Failure to allow simultaneous IX and IS states")
-	}
+	assert.True(t, m.registerIX(), "Failure to register IX state from nascent Mutex")
+	assert.True(t, m.registerIS(), "Failure to allow simultaneous IS and IX states")
 
 	// IX -> IX
 	m = New()
-	if !m.registerIX() {
-		t.Errorf("Failure to register IX state from nascent Mutex")
-	}
-	if !m.registerIX() {
-		t.Errorf("Failure to allow simultaneous IX states")
-	}
+	assert.True(t, m.registerIX(), "Failure to register IX state from nascent Mutex")
+	assert.True(t, m.registerIX(), "Failure to allow simultaneous IX states")
 }
